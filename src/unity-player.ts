5b204bccb9fcf2a541e578ff96550a40ca41a414
Ray Barrera <ray.barrera@aofl.com>
2023-01-30 23:16:30 -0800

    first working sample

--- src/unity-player.ts
+++ src/unity-player.ts
@@ -1,16 +1,45 @@
-import {LitElement, css, html} from 'lit';
+import {LitElement, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 
 @customElement('unity-player')
 export class UnityPlayer extends LitElement {
-    static styles =  css`
-    p { color: black}
-    `;
+    @property()
+    loader?: string;
 
     @property()
-    name?: string = "world";
+    container?: string;
+    
+    @property()
+    viewportWidth?: int = 100;
+    
+    @property()
+    viewportHeight?: int = 100;
+    
+    get gameContainer() {
+        return this.renderRoot.querySelector("#game-container");
+    }
+
+    constructor() {
+        super();
+        this.addEventListener('loader-loaded', (e)=> {
+            UnityLoader.instantiate(this.gameContainer, this.container);
+        });
+    }
+    
+    override connectedCallback () {
+        super.connectedCallback();
+        //this.id = "gameContainer"
+        var script = document.createElement("script");
+        script.src = this.loader;
+        script.onload = () => {
+            var ev = new Event('loader-loaded');
+            this.dispatchEvent(ev);
+        };
+        document.body.appendChild(script);
+    }
 
     render() {
-        return html`<p> Hello ${this.name}!</p>`;
+        return html`
+        <div id="game-container" style="width: ${this.viewportWidth}%; height: ${this.viewportHeight}%;"></div>`;
     }
 }
